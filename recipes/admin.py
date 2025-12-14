90c670325d3bf98c23c5c4a506d4911d86a55368
AugustinaGonja <qqwqdddj@students.codeinstitute.net>
2025-12-14 16:36:15 +0000

    Customised product and recipe admin.

--- recipes/admin.py
+++ recipes/admin.py
@@ -3,5 +3,16 @@ from .models import Recipe, Category
 
 # Register your models here.
 
+class RecipeAdmin(admin.ModelAdmin):
+    list_display = ('name', 'get_categories', 'rating')
+    search_fields = ('name', 'ingredients', 'instructions')
+    list_filter = ('categories',)
+    ordering = ('name',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'friendly_name')
+    search_fields = ('name',)
+
+
 admin.site.register(Recipe)
 admin.site.register(Category)
