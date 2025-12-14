3cfa0102600beba12bcaf6ea0f77b877aaff9b9b
AugustinaGonja <qqwqdddj@students.codeinstitute.net>
2025-12-14 16:47:50 +0000

    Added basic urls and views for products/recipes.

--- products/views.py
+++ products/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
+
+def product_list(request):
+
+    """
+    Display list of products with search, filter, and sorting
+    functionality.
+
+    """
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
--- recipes/views.py
+++ recipes/views.py
@@ -1,3 +1,19 @@
 from django.shortcuts import render
+from .models import Recipe
 
 # Create your views here.
+
+def recipe_list(request):
+
+    """
+    Display list of recipes with search, filter, and sorting
+    functionality.
+
+    """
+    recipes = Recipe.objects.all()
+
+    context = {
+        'recipes' : recipes
+    }
+
+    return render(request, 'recipes/recipes.html', context)
