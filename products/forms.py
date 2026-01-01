1437428af059bf4d2d8b82d929b05d04f9f14227
AugustinaGonja <qqwqdddj@students.codeinstitute.net>
2026-01-01 01:14:24 +0000

    Added product form for admin.

--- products/forms.py
+++ products/forms.py
@@ -1,32 +1,16 @@
 from django import forms
-from .models import UserProfile
-
-# Django Form Customization
+from .models import Product, Category
 
 
-class UserProfileForm(forms.ModelForm):
+class ProductForm(forms.ModelForm):
     class Meta:
-        model = UserProfile
-        exclude = ('user',)
+        model = Product
+        fields = '__all__'
 
     def __init__(self, *args, **kwargs):
-        """Remove labels and replace them with placeholders."""
+        """Set category choices to friendly names."""
         super().__init__(*args, **kwargs)
-        placeholders = {
-            'default_contact_number': 'Contact Number',
-            'default_town_or_city': 'Town or City',
-            'default_address_line_1': 'Address Line 1',
-            'default_address_line_2': 'Address Line 2',
-            'default_post_code': 'Post Code',
-            'default_county': 'County',
-            'default_country': 'Country',
-        }
+        categories = Category.objects.all()
+        friendly_names = [(c.id, c.get_friendly_name()) for c in categories]
 
-        for field in self.fields:
-            if self.fields[field].required:
-                placeholder = f'{placeholders[field]} *'
-            else:
-                placeholder = placeholders[field]
-
-            self.fields[field].widget.attrs['placeholder'] = placeholder
-            self.fields[field].label = False
+        self.fields['category'].choices = friendly_names
